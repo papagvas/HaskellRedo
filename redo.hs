b20fd62e33717914c71fc166271b7e4bb78a9fd5
Gvasalia M <papagvasexy@gmail.com>
2020-09-25 18:44:44 +0300

    Added temporary file creation

--- redo.hs
+++ redo.hs
@@ -1,5 +1,21 @@
-import System.Process
+import System.Directory (renameFile, removeFile)
+import System.Exit (ExitCode(..))
+import System.Process (createProcess, waitForProcess, shell)
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
 
+main :: IO ()
 main = do
-  _ <- createProcess $ shell "bash redo.do"
-  return () 
+  args <- getArgs
+  mapM_ redo args
+
+redo :: String -> IO ()
+redo target = do
+  let tmp = target ++ "---redoing"
+  (_, _, _, prHandle) <- createProcess $ shell $ "bash " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
+  exit <- waitForProcess prHandle 
+  case exit of 
+    ExitSuccess -> do renameFile tmp target
+    ExitFailure code -> do hPutStrLn stderr $ "Redo script exited w/ non-zero exit code: " ++ show code 
+                           removeFile tmp
+  
