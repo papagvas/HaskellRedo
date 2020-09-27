6456465bc755fee8cd9152078de764da4d014f45
Gvasalia M <papagvasexy@gmail.com>
2020-09-27 22:23:56 +0300

    added REDO_TARGET for new features, refactored code a bit

--- redo.do
+++ redo.do
@@ -1,1 +1,2 @@
+redo-ifchanged redo.hs
 ghc -o $3 redo.hs 
--- redo.hs
+++ redo.hs
@@ -1,24 +1,27 @@
 import Control.Monad (filterM)
+import Data.Map.Lazy (fromList, toList, adjust, insert)
 import System.Directory (doesFileExist, renameFile, removeFile)
 import System.Exit (ExitCode(..))
-import System.FilePath (hasExtension, replaceBaseName)
-import System.Process (createProcess, waitForProcess, shell)
-import System.Environment (getArgs)
+import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
+import System.Process (createProcess, waitForProcess, shell, CreateProcess(..))
+import System.Environment (getArgs, getEnvironment)
 import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  mapM_ redo args
+main = getArgs >>= mapM_ redo
+  
+  
 
 redo :: String -> IO ()
 redo target = do
   let tmp = target ++ "---redoing"
   maybePath <- redoPath target
+  oldEnv <- getEnvironment
+  let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
   case maybePath of
     Nothing -> error $ "No .do files found for target '" ++ target ++ "'"
     Just path -> do
-      (_, _, _, prHandle) <- createProcess $ shell $ "bash " ++ path ++ " - - " ++ tmp ++ " > " ++ tmp
+      (_, _, _, prHandle) <- createProcess $ (shell $ unwords ["sh", path, ".do", "0", takeBaseName target, tmp, ">", tmp]) { env = Just newEnv } 
       exit <- waitForProcess prHandle 
       case exit of 
         ExitSuccess -> do renameFile tmp target
